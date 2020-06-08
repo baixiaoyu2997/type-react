f21ae5ace652bad5c923f4a9ef6a0c0e6f8a8837
L.Rain <bxy2997@sina.com>
2020-06-08 21:51:11 +0800

    二、函数

--- src/App.tsx
+++ src/App.tsx
@@ -1,146 +1,41 @@
-import React, { useState, useRef } from "react";
+import React, {useState, useRef } from "react";
 import {
   List,
   Avatar,
   Button,
   Typography,
   Form,
-  Input,
-  Select,
-  DatePicker,
   Menu,
   Dropdown,
   Tabs
 } from "antd";
+
 import { DownOutlined } from "@ant-design/icons";
+
+import TodoInput from './TodoInput'
+import TodoList from './TodoList'
+import { todoListData } from "./utils/data";
 
 import "./App.css";
 import logo from "./logo.svg";
 
 const { Title } = Typography;
-const { Option } = Select;
 const { TabPane } = Tabs;
 
-const todoListData = [
-  {
-    content: "图雀社区：汇聚精彩的免费实战教程",
-    user: "mRcfps",
-    time: "2020年3月2日 19:34",
-    isCompleted: false
-  },
-  {
-    content: "图雀社区：汇聚精彩的免费实战教程",
-    user: "pftom",
-    time: "2020年3月2日 19:34",
-    isCompleted: false
-  },
-  {
-    content: "图雀社区：汇聚精彩的免费实战教程",
-    user: "Holy",
-    time: "2020年3月2日 19:34",
-    isCompleted: false
-  },
-  {
-    content: "图雀社区：汇聚精彩的免费实战教程",
-    user: "crxk",
-    time: "2020年3月2日 19:34",
-    isCompleted: false
-  },
-  {
-    content: "图雀社区：汇聚精彩的免费实战教程",
-    user: "Pony",
-    time: "2020年3月2日 19:34",
-    isCompleted: false
-  }
-];
-
-const userList = [
-  {
-    id: "666666666",
-    name: "图雀社区",
-    avatar: "https://avatars0.githubusercontent.com/u/39240800?s=60&v=4"
-  },
-  {
-    id: "23410977",
-    name: "mRcfps",
-    avatar: "https://avatars0.githubusercontent.com/u/23410977?s=96&v=4"
-  },
-  {
-    id: "25455350",
-    name: "crxk",
-    avatar: "https://avatars1.githubusercontent.com/u/25455350?s=96&v=4"
-  },
-  {
-    id: "23410977",
-    name: "pftom",
-    avatar: "https://avatars0.githubusercontent.com/u/23410977?s=96&v=4"
-  },
-  {
-    id: "58352313",
-    name: "holy",
-    avatar: "https://avatars0.githubusercontent.com/u/58352313?s=96&v=4"
-  }
-];
-
-const menu = (
-  <Menu>
-    <Menu.Item>完成</Menu.Item>
-    <Menu.Item>删除</Menu.Item>
-  </Menu>
-);
-
-const TodoInput = ({ value = {} }) => {
-  return (
-    <div className="todoInput">
-      <Input type="text" placeholder="输入待办事项内容" />
-      <Select style={{ width: 80 }} size="small" defaultValue="666666666">
-        {userList.map(user => (
-          <Option value={user.id}>{user.name}</Option>
-        ))}
-      </Select>
-      <DatePicker
-        size="small"
-        style={{ marginLeft: "16px", marginRight: "16px" }}
-      />
-    </div>
-  );
-};
-
-function TodoList() {
-  return (
-    <List
-      className="demo-loadmore-list"
-      itemLayout="horizontal"
-      dataSource={todoListData}
-      renderItem={item => (
-        <List.Item
-          actions={[
-            <Dropdown overlay={menu}>
-              <a key="list-loadmore-more">
-                操作 <DownOutlined />
-              </a>
-            </Dropdown>
-          ]}
-        >
-          <List.Item.Meta
-            avatar={
-              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-            }
-            title={<a href="https://ant.design">{item.user}</a>}
-            description={item.time}
-          />
-          <div>{item.content}</div>
-        </List.Item>
-      )}
-    />
-  );
-}
-
 function App() {
+  const [todoList, setTodoList] = useState(todoListData);
+
   const callback = () => {};
 
-  const onFinish = () => {};
+  const onFinish = (values: any) => {
+    const newTodo = { ...values.todo, isCompleted: false };
+    setTodoList(todoList.concat(newTodo));
+  };
+
   const ref = useRef(null);
+
+  const activeTodoList = todoList.filter((todo:any) => !todo.isCompleted);
+  const completedTodoList = todoList.filter((todo:any) => todo.isCompleted);
 
   return (
     <div className="App" ref={ref}>
@@ -153,18 +48,23 @@ function App() {
           <Form.Item name="todo">
             <TodoInput />
           </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              提交
+            </Button>
+          </Form.Item>
         </Form>
       </div>
       <div className="container">
-        <Tabs onChange={callback} type="card">
+      <Tabs onChange={callback} type="card">
           <TabPane tab="所有" key="1">
-            <TodoList />
+            <TodoList todoList={todoList} />
           </TabPane>
           <TabPane tab="进行中" key="2">
-            <TodoList />
+            <TodoList todoList={activeTodoList} />
           </TabPane>
           <TabPane tab="已完成" key="3">
-            <TodoList />
+            <TodoList todoList={completedTodoList} />
           </TabPane>
         </Tabs>
       </div>
